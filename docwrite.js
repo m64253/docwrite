b057b076edd04df0f598e407ab0602c9b19b37a4
Mikael Abrahamsson <mikael.abrahamsson@gmail.com>
2011-05-07 10:33:26 +0200

    Resistance is futile :(

--- docwrite.js
+++ docwrite.js
@@ -1,33 +1,63 @@
 (function(win){
 	var doc = win.document,
-		docWrite = doc.write, // TODO: Figure out when to give back the write function??
+		originalDocWrite = doc.write, // TODO: Figure out when to give back the write function??
 		buffer = [],
 		dependencies = [],
 		isLoaded = false,
 		orphanText = false;
 	
-	function objectKeys(obj) {
-		var key,
-			keys = [];
-		
-		if (Object.keys) {
-			keys = Object.keys(obj);
-		} else {
-			for (key in obj) {
-				if (obj.hasOwnProperty(key)) {
-					keys.push(key);
-				}
-			}
+	function logger() {
+		if (!logger.el) {
+			logger.el = doc.createElement('ul');
+			logger.el.style.position = 'absolute';
+			logger.el.style.top = '0';
+			logger.el.style.left = '0';
+			logger.el.style.width = '200px';
+			logger.el.style.margin = '0';
+			logger.el.style.fontFamily = 'monospace';
+			logger.el.style.fontSize = '10px';
+			doc.body.appendChild(logger.el);
 		}
 		
-		return keys;
+		var args = Array.prototype.slice.call(arguments),
+			text,
+			li,
+			match = (args[1] || '').match(/(multi|echo).php\?t=dependency-text-([0-9])/);
+		
+		for (var i=0; i < args.length; i++) {
+			args[i] = args[i].replace(/</g, '&lt;').replace(/>/g, '&gt;');
+		}
+		
+		if (match) {
+			args[0] += ' - ' + match[1] + ' ' + match[2];
+		}
+		
+		text = args.join('<br />') + '<hr />';
+		
+		if (text.match('dependency')) {
+			li = doc.createElement('li');
+			li.style.paddingBottom = '5px';
+			li.innerHTML = text;
+			logger.el.appendChild(li);
+		}
+		
+	}
+	
+	function counter(max, fn) {
+		var i = 0;
+		return function counting() {
+			i++;
+			if (i >= max) {
+				fn();
+			}
+		};
 	}
 	
 	function bindLoad(el, fn) {
 		if (el.addEventListener) {
 			el.addEventListener('load', fn, false);
-		//} else if (el.attachEvent) {
-		//	el.attachEvent('onload', fn);
+		} else if (el.tagName !== 'SCRIPT' && el.attachEvent) {
+			el.attachEvent('onload', fn);
 		} else {
 			el.onreadystatechange = function() {
 				var state = el.readyState;
@@ -41,13 +71,14 @@
 	function unbindLoad(el, fn) {
 		if (el.removeEventListener) {
 			el.removeEventListener('load', fn, false);
-		//} else if (el.detachEvent) {
-		//	el.detachEvent('onload', fn);
+		} else if (el.tagName !== 'SCRIPT' && el.detachEvent) {
+			el.detachEvent('onload', fn);
 		} else {
 			el.onreadystatechange = null;
 		}
 	}
 	
+	// Create A DOM element and bind some load event listeners
 	function createLoadableElement(tagName, fn, keepInDOM) {
 		var el = doc.createElement(tagName),
 			handler = function() {
@@ -71,6 +102,58 @@
 		return el;
 	}
 	
+	// Load file
+	function loadFile(files, fn) {
+		var len = files.length,
+			i,
+			doneFn = counter(len, fn);
+		
+		if (len === 0) {
+			fn();
+		} else {
+			for (i=0; i < len; i++) {
+				loadFile.js(files[i], doneFn);
+			}
+		}
+	}
+	loadFile.pre = function loadFilePre(data, fn) {
+		var el = createLoadableElement('object', fn);
+		
+		el.data = data;
+		el.type = "text/plain"; // Needed for IE
+		el.width = 1; // Needed for IE
+		el.height = 1; // Needed for IE
+		
+		// Styles
+		el.style.visibility = "hidden";
+		el.style.position = 'absolute';
+		el.style.top = '-1000px';
+		el.style.left = '-1000px';
+		
+		body.appendChild(el);
+	};
+	loadFile.js = function loadFileJS(src, fn) {
+		var el = createLoadableElement('script', fn);
+		el.src =  src;
+		doc.body.appendChild(el);
+	};
+	loadFile.css = function loadFileCSS(href, fn) {
+		loadFile.pre(href, function(){
+			var el = doc.createElement('link');
+			
+			el.rel = 'stylesheet';
+			el.type = 'text/css';
+			el.href = href;
+			
+			doc.body.appendChild(el);
+			
+			fn.apply(el, arguments);
+		});
+	};
+	
+	// Create wrapping DOM element to innerHTML
+	// the document.write content
+	var pp = 0
 	function createWrapper(text) {
 		var el = doc.createElement('div'),
 			scripts,
@@ -79,78 +162,91 @@
 			len,
 			i;
 		
-		el.innerHTML = '<span>IE is dumb</span>' + text;
-		
+		// IE seems reluctant to create the DOM if text is just a script tag
+		el.innerHTML = '<span>IE is the awesomest browser evar!</span>' + text;
+				
 		scripts = el.getElementsByTagName('script');
 		
-		if (scripts.length !== 0) {
-			for (i=0, len=scripts.length; i < len; i++) {
-				script = scripts[i];
+		for (i=0, len=scripts.length; i < len; i++) {
+			script = scripts[i];
+			
+			newScript = createLoadableElement('script', function(el) {
 				
-				newScript = createLoadableElement('script', function(){
+				logger('LOADED', this.src);
+				
+				if (orphanText) {
 					
-					if (orphanText) {
-						buffer.push({
-							text: orphanText,
-							el: newScript
-						});
-						orphanText = null;
-					}
+					//logger('TEXT', orphanText);
+					
+					buffer.push({
+						text: orphanText,
+						el: this
+					});
+					orphanText = null;
 					
 					processBuffer();
-					
-				}, true);
-				newScript.src = script.src;
+				}
 				
-				script.parentNode.replaceChild(newScript, script);
-			}
+			}, true);
+			newScript.defer = true;
+			newScript.src = script.src;
+			
+			script.parentNode.replaceChild(newScript, script);
 		}
 		
+		// Remove IE fix span
 		el.removeChild(el.getElementsByTagName('span')[0]);
 		
 		return el;
 	}
 	
+	// Process Buffer
 	function processBuffer(){
 		
-		var item = buffer.shift(),
+		var	item = buffer.shift(),
 			wrapper;
 		
 		if (item) {
-			wrapper = createWrapper(item.text);
 			
+			logger('PROCESS BUFFER', item.text);
+			
+			wrapper = createWrapper(item.text);
 			item.el.parentNode.replaceChild(wrapper, item.el);
 		}
 		
-		if (buffer.length !== 0) {
+		//if (buffer.length !== 0) {
 			setTimeout(processBuffer, 0);
-		}
+		//}
 	}
-	
-	
-	
-	function counter(max, fn) {
-		var i = 0;
-		return function counting() {
-			i++;
-			if (i === max) {
-				fn();
-			}
-		};
-	}
-	
+		
 	function processDependencies() {
 		var deps = dependencies.concat(),
+			dep,
 			len = deps.length,
 			i,
-			whenAllIsSadAndDone = counter(len, processBuffer);
+			doneFn;
 		
-		for (i=0; i < len; i++) {
-			load(deps[i], whenAllIsSadAndDone);
+		if (len === 0) {
+			processBuffer();
+		} else {
+			doneFn = counter(len, function(){
+				processBuffer();
+			});
+			
+			for (i=0; i < len; i++) {
+				dep = deps[i];
+				loadFile(dep.files, (function(fn){
+					return function() {
+						fn();
+						doneFn();
+					};
+				}(dep.fn)));
+			}
 		}
 	}
 	
-	function proxyDocWrite(text) {
+	// So this is it?
+	function docWrite(text) {
 		
 		var scripts,
 			el,
@@ -159,7 +255,11 @@
 			i;
 		
 		if (isLoaded) {
+			
+			logger('WRITE', text)
+			
 			orphanText = (orphanText || '') + text;
+			
 		} else {
 			scripts = doc.getElementsByTagName('script'),
 			el = scripts[scripts.length - 1];
@@ -182,25 +282,37 @@
 			}
 		}
 	};
-	proxyDocWrite.dependency = proxyDocWrite.dep = proxyDocWrite.require = function dependency(deps, fn) {
+	
+	// Dear child has many names
+	docWrite.dependency = docWrite.dep = docWrite.require = function dependency() {
 		var args = Array.prototype.slice.call(arguments);
+			fn = args.pop(),
+			files = args.shift() || [];
+		
+		if (typeof(fn) !== 'function') {
+			files = fn;
+			fn = function(){};
+		}
+		
 		dependencies.push({
-			deps: deps,
+			files: files,
 			fn: fn
 		});
 	};
 	
-	doc.write = proxyDocWrite;
+	// Overwrite the global document write
+	doc.write = docWrite;
 	
+	// Bing a listener to window load event
 	if (win.addEventListener) {
 		win.addEventListener('load', function onload(){
 			isLoaded = true;
-			processBuffer();
+			processDependencies();
 		}, false);
 	} else if (win.attachEvent) {
 		win.attachEvent('onload', function onload(){
 			isLoaded = true;
-			processBuffer();
+			processDependencies();
 		}, false);
 	}
 }(this));
